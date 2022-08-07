8c12e739e3734b8c9662f081eb971739d53425f3
Daniel C. Bucher <daniel.bucher88@gmail.com>
2022-08-07 14:43:45 -0300

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { StaticImage } from "gatsby-plugin-image";
 
 import Layout from "../components/layout";
 
@@ -6,6 +7,10 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Me lying on a bed with a tabby cat on my back and an orange cat by my side."
+        src="../images/eu_peto_e_farofa.jpg"
+      />
     </Layout>
   );
 };
